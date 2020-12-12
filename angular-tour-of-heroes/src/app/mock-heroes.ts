0bf82d3a90cfba88f7d6489448a40c88a8944be0
EricJGardner <74524075+EricJGardner@users.noreply.github.com>
2020-12-11 18:00:44 -0600

    first commit
    completed first 2 steps of Tour of Heroes Angular project with some color changes in the CSS and added 'power' and 'age' fields to heroes list components

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Dr Nice', power: 'Kindness', age: 37 },
+  { id: 2, name: 'Narco', power: 'Drugs', age: 24 },
+  { id: 3, name: 'Bombasto', power: 'Explosions', age: 66 },
+  { id: 4, name: 'Celeritas', power: 'Veggies', age: 64 },
+  { id: 5, name: 'Magneta', power: 'Magnetism', age: 53 },
+  { id: 6, name: 'RubberMan', power: 'Stretchiness', age: 32 },
+  { id: 7, name: 'Dynama', power: 'Charisma', age: 55 },
+  { id: 8, name: 'Dr IQ', power: 'Smarts', age: 197 },
+  { id: 9, name: 'Magma', power: 'Liquid Lava', age: 672 },
+  { id: 10, name: 'Tornado', power: 'Passing Wind', age: 124 }
+];
